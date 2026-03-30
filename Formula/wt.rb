b5205b12a5f776cfd4265e2075bcadeecb0cea84
Ashwini Chaudhary <ashwch3018@gmail.com>
2026-03-30 16:40:59 -0400

    wt 0.1.1

--- Formula/wt.rb
+++ Formula/wt.rb
@@ -1,8 +1,8 @@
 class Wt < Formula
   desc "Interactive git worktree dashboard with fzf TUI"
   homepage "https://github.com/ashwch/wt"
-  url "https://github.com/ashwch/wt/archive/refs/tags/v0.1.0.tar.gz"
-  sha256 "c1c7d4abd339f5d9dd3ca6718bca078ff2ab9d40199e6f5619213176794a9b70"
+  url "https://github.com/ashwch/wt/archive/refs/tags/v0.1.1.tar.gz"
+  sha256 "7b9d9e7d057aaba9a1d62389f57d00e302a30b8777e75af1fc8f3f5ec7355c6c"
   license "MIT"
   head "https://github.com/ashwch/wt.git", branch: "main"
 
